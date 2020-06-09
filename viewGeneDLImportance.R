686bcc8a769703829c43615d72fe502ebd35442a
gaoshang1102 <65426274+gaoshang1102@users.noreply.github.com>
2020-06-09 10:41:49 -0500

    Create viewGeneDLImportance.R

---
+++ viewGeneDLImportance.R
@@ -0,0 +1,23 @@
+view_individual_gene_importance <- function(importance_matrix, geneName){
+  tmp <- matrix(unname(unlist(importance_matrix[geneName. ])), nrow = 2500, byrow = F)
+  df <- data.frame(site =seq(1, 2500))
+  df$DNase <- tmp[, 1]
+  df$H3K4me1 <- tmp[, 2]
+  df$H3K4me3 <- tmp[, 3]
+  df$H3K9me3 <- tmp[, 4]
+  df$H3K27me3 <- tmp[, 5]
+  df$H3K36me3 <- tmp[, 6]
+  df$WGBS <- tmp[, 7]
+  df$DNase <- loess(df$DNase ~ df$site, span=0.1)$fitted
+  df$H3K4me1 <-  loess(df$H3K4me1 ~ df$site, span=0.1)$fitted
+  df$H3K4me3 <-  loess(df$H3K4me3 ~ df$site, span=0.1)$fitted
+  df$H3K9me3 <-  loess(df$H3K9me3 ~ df$site, span=0.1)$fitted
+  df$H3K27me3 <-  loess(df$H3K27me3 ~ df$site, span=0.1)$fitted
+  df$H3K36me3 <-  loess(df$H3K36me3 ~ df$site, span=0.1)$fitted
+  df$WGBS <-  loess(df$WGBS ~ df$site, span=0.1)$fitted
+  df <- gather(df, "epi", "importance", 2:8)
+  df$epi <- factor(df$epi, levels = c("DNase", "H3K4me1", "H3K4me3", "H3K9me3", "H3K27me3", "H3K36me3", "WGBS"))
+  plots <- ggplot(df, aes(site, importance, color = epi)) +
+          geom_point() + theme_bw()
+  return(plots)
+ }
