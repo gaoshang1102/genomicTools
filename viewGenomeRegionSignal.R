28c60ed19772380a971a6d7ab61512070c2a1420
gaoshang1102 <65426274+gaoshang1102@users.noreply.github.com>
2020-06-09 14:36:42 -0500

    Update viewGenomeRegionSignal.R

--- viewGenomeRegionSignal.R
+++ viewGenomeRegionSignal.R
@@ -1,5 +1,5 @@
 View_signal <- function(data, geneName){
   df <- data.frame(sites = seq(1, 2500))
-  df$signal <- data[geneName, ]
+  df$signal <- unlist(data[geneName, ])
   ggplot(df, aes(sites, signal)) + geom_point() + theme_bw()
 }
